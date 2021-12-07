1f7092d01762cd0771b111bd0ab6f5f9053c65c6
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2021-12-07 13:47:44 +0000

    [Add] ジャンル画面作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,30 @@
 class Admin::GenresController < ApplicationController
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+      flash[:notice] = "ジャンル投稿が完了しました"
+      redirect_to admin_genres_path
+  end
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genres_params)
+    redirect_to admin_genres_path
+  end
+
+  private
+  #ストロングパラメーター
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
 
+  root to: 'public/homes#top'
+  
+
   namespace :admin do
     resources :homes
     resources :genres, only: [:index, :create, :edit, :update]
