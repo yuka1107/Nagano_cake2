371e95647bd61e78fe4ea23bf1fe1b367fdd95ea
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2021-12-07 13:02:26 +0000

    [Add]コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins,controllers: {sessions: 'admins/sessions' }
+
+  devise_for :admins
   devise_for :customers
+
+  namespace :admin do
+    resources :homes
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items
+    resources :customers
+  end
 
 end
