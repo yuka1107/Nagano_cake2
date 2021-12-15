0ce115022f5cf80e3a7bccd235cfda7f6cd6fced
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2021-12-15 15:34:44 +0000

    [Add]レイアウト調整

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5f8f2dc74ceab3ca33f2df3e70d6cf3c88c7d9f7baa653660ce149c5a5223618811b3c3249ebab531bb16f3d4f9afa2daaa93cef4970f37f5ccfc0a47ceaddb8'
